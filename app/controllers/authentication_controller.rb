374b8bac8949b0669c1f55ad9f42197d469d1178
Erick Contreras <er.contreras@hotmail.com>
2023-04-03 18:46:42 -0600

    Add API configuration

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,13 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_user
+
+  def login
+    @user = User.find_by_email(params[:user][:email])
+    if @user&.authenticate(params[:user][:password])
+      token = jwt_encode(user_id: @user.id)
+      render json: { user: @user.email, token: token }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+end
