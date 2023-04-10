550f26ad665ee2fecabcf6aa1e4a27af3055c533
Erick Contreras <er.contreras@hotmail.com>
2023-04-10 16:02:53 -0600

    Change header variable to 'request.headers['HTTP_AUTHORIZATION']' in order to get the JWT Bearer token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_user
-    header = request.headers['Authentication']
+    header = request.headers['HTTP_AUTHORIZATION']
     header = header.split(' ').last if header
     decoded = jwt_decode(header)
     @current_user = User.find(decoded[:user_id])
