38c36fa109ac9db353b6b534b32340266fcb1d52
Erick Contreras <er.contreras@hotmail.com>
2023-05-01 20:18:16 -0600

    add assocition between user and bill modules

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,6 @@
 class Bill < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :bills, dependent: :destroy
+
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
