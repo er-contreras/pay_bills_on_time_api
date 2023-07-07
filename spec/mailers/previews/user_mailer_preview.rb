7265f8cd4281c89af74234d4e62c28e77ed52b66
Erick Contreras <er.contreras@hotmail.com>
2023-07-06 19:50:16 -0600

    Create welcome_email and fix preview

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
-    user = params[:user]
-    UserMailer.with(user: user).welcome_email
+    UserMailer.with(user: User.first).welcome_email
   end
 end
