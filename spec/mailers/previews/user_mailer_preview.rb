2081a1bccb8fea715ea1884fa783c1e78e27f835
Erick Contreras <er.contreras@hotmail.com>
2023-07-13 16:06:06 -0600

    Change the url in preview email

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user
+# Preview all emails at http://localhost:3000/rails/mailers/
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
     UserMailer.with(user: User.first).welcome_email
