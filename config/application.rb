818e1f27aea2d16e77f2d236a424d6a437b286a7
Erick Contreras <er.contreras@hotmail.com>
2023-07-24 21:19:20 -0600

    Remove comments

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,7 @@ Bundler.require(*Rails.groups)
 module TimeManagementBe
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
-    # This also configures session_options for use below
     config.session_store :cookie_store, key: '_interslice_session'
-    # Required for all session management (regardless of session_store)
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
 
