db72c017487742f81ab96304e97db16a7e1b1c60
Erick Contreras <er.contreras@hotmail.com>
2023-07-25 18:15:26 -0600

    Implement perform method for email notification

--- app/sidekiq/email_job.rb
+++ app/sidekiq/email_job.rb
@@ -1,20 +1,16 @@
 class EmailJob
   include Sidekiq::Job
 
-  # bill_hour = Time.new(2023, 7, 22, 14, 30, 0)
-  def perform(bill_hour)
-    bill_hour = DateTime.strptime(bill_hour, '%H:%M')
-    current_time = '21:50'
+  def perform(bill_date)
+    day, month, year = bill_date.split('/').map(&:to_i)
+    date = Date.new(year, day, month)
+    date_plus_one = date + 1
+    date_plus_one_str = date_plus_one.strftime('%d/%m/%y')
 
-    if current_time < bill_hour
-      # When using perform_at, the first argument should be the time when the job should be performed,
-      # and the second argument should be the job arguments to be passed to the perform method.
-      self.class.perform_at(bill_hour, nil)
-    else
-      self.class.perform_in(1.minute, current_time + 1.minute)
-    end
-    # Send email to the user
-    # UserMailer.send_bill_notification(bill_date).deliver
-    puts "Bill notification email has been sent to the user for #{bill_hour}"
+    self.class.perform_in(1.day, date_plus_one_str)
+
+    UserMailer.with(User.first).welcome_email.deliver_now
+
+    puts "Bill notification email has been sent to the user no time: #{date_plus_one_str}"
   end
 end
