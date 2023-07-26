cdfe8ac8396e9fed771de00ccf8d4033725e7954
Erick Contreras <er.contreras@hotmail.com>
2023-07-25 18:13:53 -0600

    Handle @user when nil

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,11 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = 'http://localhost:5000/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+
+    if @user.nil?
+      mail(to: User.first.email, subject: 'Welcome to My Awesome Site')
+    else
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    end
   end
 end
