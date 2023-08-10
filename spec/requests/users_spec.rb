38d47bcd10f5f8da7b32c5d68ecc23c8334d8840
Erick Contreras <er.contreras@hotmail.com>
2023-08-09 21:31:35 -0600

    Add tests when valid and invalid token on get request

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,63 @@
 require 'rails_helper'
+require 'support/factory_bot'
 
-RSpec.describe 'Users', type: :controller do
-  describe 'GET /index' do
-    it 'returns http success'
+RSpec.describe UsersController, type: :controller do
+  include JsonWebToken
+
+  controller do
+    def index
+      render json: { message: 'Success' }
+    end
+  end
+
+  describe 'authentication' do
+    let(:user) { create(:user) }
+    let(:valid_token) { jwt_encode(user_id: user.id) }
+    let(:invalid_token) { 'this_is_not_a_valid_token' }
+
+    context 'with a valid token' do
+      before do
+        request.headers['Authorization'] = "Bearer #{valid_token}"
+        get :index
+      end
+
+      it 'sets the @current_user' do
+        expect(controller.instance_variable_get(:@current_user)).to eq(user)
+      end
+
+      it 'returns a successful response' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with an invalid token' do
+      before do
+        request.headers['Authorization'] = "Bearer #{invalid_token}"
+        controller.instance_variable_set(:@current_user, nil)
+        get :index
+      end
+
+      it 'does not set the @current_user' do
+        expect(controller.instance_variable_get(:@current_user)).to be_nil
+      end
+
+      it 'returns an unauthorized response' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'without a token' do
+      before do
+        get :index
+      end
+
+      it 'does not set the @current_user' do
+        expect(controller.instance_variable_get(:@current_user)).to be_nil
+      end
+
+      it 'returns an unauthorized response' do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
   end
 end
