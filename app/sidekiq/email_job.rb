3eabed583e476b86601181d05b2561cf707f0932
Erick Contreras <er.contreras@hotmail.com>
2023-08-14 19:36:20 -0600

    Add email parameter to perform method in EmailJob

--- app/sidekiq/email_job.rb
+++ app/sidekiq/email_job.rb
@@ -1,16 +1,18 @@
 class EmailJob
   include Sidekiq::Job
 
-  def perform(bill_date)
+  sidekiq_options retry: false
+
+  def perform(email, bill_date)
     day, month, year = bill_date.split('/').map(&:to_i)
-    date = Date.new(year, day, month)
+    date = Date.new(year, month, day)
     date_plus_one = date + 1
     date_plus_one_str = date_plus_one.strftime('%d/%m/%y')
 
-    self.class.perform_in(1.day, date_plus_one_str)
+    self.class.perform_in(1.day, email, date_plus_one_str)
 
-    UserMailer.with(User.first).welcome_email.deliver_now
+    MonthlyBillNotificationMailer.monthly_bill_notification(email, date_plus_one_str).deliver_now
 
-    puts "Bill notification email has been sent to the user no time: #{date_plus_one_str}"
+    puts "Bill notification email has been sent to the user: #{date_plus_one_str}"
   end
 end
