d18f050d8ab2b115d0c46be0567c3ba435532a15
Erick Contreras <er.contreras@hotmail.com>
2023-08-23 21:43:28 -0600

    Refactor: remove time conversion as we already performing at a certain time

--- app/sidekiq/email_job.rb
+++ app/sidekiq/email_job.rb
@@ -4,15 +4,8 @@ class EmailJob
   sidekiq_options retry: false
 
   def perform(email, bill_date)
-    day, month, year = bill_date.split('/').map(&:to_i)
-    date = Date.new(year, month, day)
-    date_plus_one = date + 1
-    date_plus_one_str = date_plus_one.strftime('%d/%m/%y')
+    MonthlyBillNotificationMailer.monthly_bill_notification(email, bill_date).deliver_later
 
-    self.class.perform_in(1.day, email, date_plus_one_str)
-
-    MonthlyBillNotificationMailer.monthly_bill_notification(email, date_plus_one_str).deliver_now
-
-    puts "Bill notification email has been sent to the user: #{date_plus_one_str}"
+    puts "Bill notification email will be sent to the user on: #{bill_date}"
   end
 end
