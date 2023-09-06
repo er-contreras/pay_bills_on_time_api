f665f6509ee5c9d21a764733e4c6709b755fa1fb
Erick Contreras <er.contreras@hotmail.com>
2023-09-06 16:10:58 -0600

    Refactor: remove not related comment

--- app/sidekiq/email_job.rb
+++ app/sidekiq/email_job.rb
@@ -5,7 +5,5 @@ class EmailJob
 
   def perform(email, bill_date)
     MonthlyBillNotificationMailer.monthly_bill_notification(email, bill_date).deliver_later
-
-    puts "Bill notification email will be sent to the user on: #{bill_date}"
   end
 end
