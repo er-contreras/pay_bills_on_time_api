ce49dde06a74992b8ebe942cb1d426f0c13df005
Erick Contreras <er.contreras@hotmail.com>
2023-09-11 22:18:47 -0600

    Feat: toggle a notification from bill_notifications reacord whhen user click on the notification button

--- app/controllers/bill_notification_controller.rb
+++ app/controllers/bill_notification_controller.rb
@@ -1,14 +1,21 @@
 class BillNotificationController < ApplicationController
   def create
-    bill_date = bill_notification_params[:date]
-    email = bill_notification_params[:userEmail]
+    bill_name = bill_notification_params[:name]
+    bill = Bill.find(bill_notification_params[:id])
 
-    EmailJob.perform_at(bill_date.to_datetime, email, bill_date)
+    if bill
+      # Toggle the bill_notification column
+      BillNotification.find_by(bill_id: bill.id).toggle(:notification).save
 
-    render json: {
-      message:
-        "Bill notification request has been received to Date: #{bill_date}, Email: #{email}"
-    }
+      render json: {
+        message: "Bill notifications are now for #{bill_name}"
+      }
+    else
+      render json: {
+        message: "User with email #{bill_name} not found.",
+        bill_notification_state: nil
+      }, status: :not_found
+    end
   end
 
   private
