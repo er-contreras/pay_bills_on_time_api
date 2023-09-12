98173a8d1a8ec8fd3791067d5af578d1317292ce
Erick Contreras <er.contreras@hotmail.com>
2023-09-11 22:25:36 -0600

    Feat: configure perfomr from EmailJob to schedule a user email based on bill notification validations and user date

--- app/sidekiq/email_job.rb
+++ app/sidekiq/email_job.rb
@@ -1,9 +1,19 @@
 class EmailJob
-  include Sidekiq::Job
+  include Sidekiq::Worker
 
   sidekiq_options retry: false
 
-  def perform(email, bill_date)
-    MonthlyBillNotificationMailer.monthly_bill_notification(email, bill_date).deliver_later
+  def perform
+    current_date = Date.today
+    bills_notifications = BillNotification.where(notification: true).includes(:bill)
+
+    bills_notifications.each do |bill_notification|
+      user = bill_notification.bill.user
+      date = bill_notification.bill.date
+
+      if date.to_date == current_date
+        MonthlyBillNotificationMailer.monthly_bill_notification(user.email, current_date).deliver_later
+      end
+    end
   end
 end
