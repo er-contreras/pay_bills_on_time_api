11aa0e55e37045e7eb94ec9feab8ed446940c0a2
Erick Contreras <er.contreras@hotmail.com>
2023-09-11 22:20:48 -0600

    Refactor: remove comments as they are redudant

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,7 +1,6 @@
 class BillsController < ApplicationController
   before_action :set_bill, only: %i[show update destroy]
 
-  # GET /bills
   def index
     @bills = Bill.all
 
@@ -11,29 +10,22 @@ class BillsController < ApplicationController
   def user_bills
     @user_bills = Bill.where(user_id: params[:user_id])
 
-
-    # Iterate over each bill
     @user_bills.each do |bill|
       deadline_date = Date.strptime(bill.date, '%d/%m/%Y')
 
       next unless deadline_date < Date.current
 
-      # Calculate the new deadline date
       new_deadline_date = deadline_date.advance(months: 1)
-
-      # Update the bill's deadline date
       bill.update(date: new_deadline_date.strftime('%d/%m/%Y'))
     end
 
     render json: @user_bills
   end
 
-  # GET /bills/1
   def show
     render json: @bill
   end
 
-  # POST /bills
   def create
     @bill = Bill.new(bill_params)
 
@@ -44,7 +36,6 @@ class BillsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /bills/1
   def update
     if @bill.update(bill_params)
       render json: @bill
@@ -53,19 +44,16 @@ class BillsController < ApplicationController
     end
   end
 
-  # DELETE /bills/1
   def destroy
     @bill.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_bill
     @bill = Bill.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def bill_params
     params.require(:bill).permit(:name, :date, :user_id)
   end
