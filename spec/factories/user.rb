6fd8881955be10322b774b7d3576c717e163ad97
Erick Contreras <er.contreras@hotmail.com>
2023-09-14 21:55:23 -0600

    Feat: use factory for testing perform method in sidekiq

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,23 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "john#{n}@example.com"
+  end
+
   factory :user do
     name { 'John Doe' }
-    username { 'johndoe' }
-    email { 'john@gmail.com' }
+    sequence(:username) { |n| "johndoe#{n}" }
+    email
     password { 'password' }
+  end
+
+  factory :bill do
+    name { 'Rent' }
+    date { '10/10/2020' }
+    association :user
+  end
+
+  factory :bill_notification do
+    association :bill
+    notification { true }
   end
 end
