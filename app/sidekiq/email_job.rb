d60089b749089ca61c3c6ec45e85dad2a1df1afd
Erick Contreras <er.contreras@hotmail.com>
2023-09-14 21:54:34 -0600

    Refactor: make code more readable, maintanable and DRY

--- app/sidekiq/email_job.rb
+++ app/sidekiq/email_job.rb
@@ -4,16 +4,23 @@ class EmailJob
   sidekiq_options retry: false
 
   def perform
-    current_date = Date.today
-    bills_notifications = BillNotification.where(notification: true).includes(:bill)
+    @current_date = Date.today
+    bills_notifications = eligible_bill_notifications
 
     bills_notifications.each do |bill_notification|
-      user = bill_notification.bill.user
-      date = bill_notification.bill.date
-
-      if date.to_date == current_date
-        MonthlyBillNotificationMailer.monthly_bill_notification(user.email, current_date).deliver_later
-      end
+      send_notification_email(bill_notification)
     end
+  end
+
+  private
+
+  def eligible_bill_notifications
+    BillNotification.where(notification: true).includes(bill: :user)
+      .select { |bn| bn.bill.date.to_date == @current_date }
+  end
+
+  def send_notification_email(bill_notification)
+    user = bill_notification.bill.user
+    MonthlyBillNotificationMailer.monthly_bill_notification(user.email, @current_date).deliver_later
   end
 end
