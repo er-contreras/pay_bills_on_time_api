80eeaef70988542ccb2d882fde73d88828cb9514
Erick Contreras <er.contreras@hotmail.com>
2023-09-29 23:35:54 -0600

    Update development branch

--- spec/sidekiq/email_job_spec.rb
+++ spec/sidekiq/email_job_spec.rb
@@ -7,24 +7,27 @@ RSpec.describe EmailJob, type: :job do
     it 'sends monthly bill notifications for bills with notification on the current date' do
       # Create a sample user and a bill with notification for the current date
       user = create(:user)
-      bill = create(:bill, user: user, date: Date.today)
-      create(:bill_notification, bill: bill, notification: true)
+      bill = create(:bill, user:, date: Date.today)
+      create(:bill_notification, bill:, notification: true)
 
       # Ensure that the mailer is expected to receive :deliver_later
-      allow(MonthlyBillNotificationMailer).to receive(:monthly_bill_notification).and_return(double('Mailer', deliver_later: true))
-
+      allow(MonthlyBillNotificationMailer).to receive(:monthly_bill_notification).and_return(double(
+                                                                                               'Mailer',
+                                                                                               deliver_later: true
+                                                                                             ))
       # Perform the job
       EmailJob.new.perform
 
       # Expect the mailer to have been called once
-      expect(MonthlyBillNotificationMailer).to have_received(:monthly_bill_notification).once.with(user.email, Date.today)
+      expect(MonthlyBillNotificationMailer).to have_received(:monthly_bill_notification).once.with(user.email,
+                                                                                                   Date.today)
     end
 
     it 'does not send notifications for bills with notification turned off' do
       # Create a sample user and a bill without notification for the current date
       user = create(:user)
-      bill = create(:bill, user: user, date: Date.today)
-      create(:bill_notification, bill: bill, notification: false)
+      bill = create(:bill, user:, date: Date.today)
+      create(:bill_notification, bill:, notification: false)
 
       # Ensure that the mailer is not expected to receive :deliver_later
       allow(MonthlyBillNotificationMailer).to receive(:monthly_bill_notification)
