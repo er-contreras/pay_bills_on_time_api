d166c2447cd0837b1cdd70a442dd94591bbbd28d
Erick Contreras <er.contreras@hotmail.com>
2024-02-11 22:17:38 -0600

    Add redis and sidekiq

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,6 @@ system_default = system_default_sect\n\
 [system_default_sect]\n\
 CipherString = DEFAULT@SECLEVEL=1" >> /etc/ssl/openssl.cnf
 
+# RUN apt-get update && apt-get install -y redis-server
+
 WORKDIR /app
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # config.redis = { url: "redis://host.docker.internal:6379/0" }
 end
